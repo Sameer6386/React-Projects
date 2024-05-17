661ccb67bc7b61aafa49e7c72d5525e941244a9a
Mohd Sameer <144150708+Sameer6386@users.noreply.github.com>
2024-05-17 19:51:58 +0000

    soilwrap

---
+++ soilwrap/src/App.jsx
@@ -0,0 +1,15 @@
+import Globe from './component/data/globe.json'
+import GlobeDemo from './Globedemo'
+
+function App() {
+  
+  return (
+    <>
+    <Globe/>
+    <GlobeDemo/>
+      
+    </>
+  )
+}
+
+export default App
